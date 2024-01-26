b008d8914f0c4ea506ec3e11cc8ece922d57dfd4
bjberkow@gmail.com <you@example.com>
2024-01-26 14:29:03 -0500

    commit

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use("\api", apiRoutes);
+app.use("\", htmlRoutes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
